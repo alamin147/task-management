d87cf9894e30d7e7499b15bc083de3979e25f7a5
alamin.147 <alamin.14780@gmail.com>
2024-10-23 17:30:07 +0600

    client codes

---
+++ client/src/App.tsx
@@ -0,0 +1,8 @@
+import "./App.css";
+
+function App() {
+  return <>
+  <h1>Home page (app)</h1></>;
+}
+
+export default App;
---
+++ client/src/main.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { persistor, store } from "./redux/store.ts";
+import { PersistGate } from "redux-persist/integration/react";
+import { Toaster } from "react-hot-toast";
+import RegisterPage from "./register/Register.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      
+    ],
+  },
+  {
+    path:"/register",
+    element:<RegisterPage/>
+  }
+]);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <Toaster />
+      <PersistGate loading={null} persistor={persistor}>
+        <RouterProvider router={router}></RouterProvider>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
+);
