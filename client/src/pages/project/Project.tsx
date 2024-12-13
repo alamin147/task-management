8155216db606b33708e80c61641293b950b68acf
alamin147 <alamin.14780@gmail.com>
2024-12-13 22:15:55 +0600

    card edit

--- client/src/pages/project/Project.tsx
+++ client/src/pages/project/Project.tsx
@@ -3,6 +3,9 @@ import { useParams } from "react-router-dom";
 import { useGetSingleTaskQuery } from "@/redux/features/tasks/tasksApi";
 import { useCreateSubTaskMutation } from "@/redux/features/subtask/subtaskApi";
 import { useCreateMiniTaskMutation } from "@/redux/features/minitask/minitaskApi";
+import { FaPencil } from "react-icons/fa6";
+import { BsPencilSquare } from "react-icons/bs";
+
 import "./project.css";
 
 const Project = () => {
@@ -12,7 +15,7 @@ const Project = () => {
   const [createMiniTask] = useCreateMiniTaskMutation();
 
   const [newTitle, setNewTitle] = useState("Untitled subtask");
-
+  console.log(data);
   const handleAddSubTask = async () => {
     try {
       await createSubTask({ taskId, title: newTitle });
@@ -48,83 +51,89 @@ const Project = () => {
   return (
     <>
       <main className="m-6 overflow-hidden">
-       
         {/* Horizontally scrollable container */}
-       <div className="overflow-x-auto">
-       <div className=" pb-8 mt-6 flex gap-6"
-        style={{ height: 'calc(100vh - 140px)' }}>
-          {data?.task?.subcards?.map((card: any, i: number) => (
-            //  First Card Section
-          <div>
-              <div className=" bg-white rounded-lg shadow-md w-72 p-4 border border-gray-200">
-              <div className="w-64 mb-4 p-3 bg-gray-50 rounded-lg shadow-sm">
-                <div className="flex gap-2 mb-2">
-                  <span className="block w-5 h-1 bg-red-500 rounded"></span>
-                </div>
-
-                <h3 className="text-lg font-semibold text-gray-800 mb-4">
-                  {card?.title}
-                </h3>
-              </div>
-
-              {/*  */}
-              <div className="bg-gray-50 rounded-lg shadow-sm">
-                {/* dynamic card here */}
-
-                <div className="space-y-2 ">
-                  {data?.task?.subcards[i]?.miniTasks?.map((minicard: any) => (
-                    <div
-                      key={minicard._id}
-                      className={`p-3 rounded-lg shadow-md ${minicard.color}`}
-                    >
-                      {minicard.title}
+        <div className="overflow-x-auto">
+          <div
+            className=" pb-8 mt-6 flex gap-6"
+            style={{ height: "calc(100vh - 140px)" }}
+          >
+            {data?.task?.subcards?.map((card: any, i: number) => (
+              //  First Card Section
+              <div>
+                <div className=" bg-white rounded-lg shadow-md w-72 p-4 border border-gray-200">
+                  <div className="w-64 mb-4 p-3 bg-gray-50 rounded-lg shadow-sm">
+                    <div className="flex gap-2 mb-2">
+                      <span className="block w-5 h-1 bg-red-500 rounded"></span>
                     </div>
-                  ))}
+
+                    <h3 className="text-lg font-semibold text-gray-800 mb-4">
+                      {card?.title}
+                    </h3>
                   </div>
-              </div>
-              {/* add btn  */}
-              <div className="mt-6 w-64 mb-4 p-3  rounded-md shadow-md">
-                <div className="">
-                  <input
-                    type="text"
-                    placeholder="Add inside mini task..."
-                    value={miniTaskInputs[card._id] || ""}
-                    onChange={(e) =>
-                      handleInputChange(card._id, e.target.value)
-                    }
-                    className="w-full px-3 py-2 text-black rounded-md shadow-md hover:bg-gray-50  active:border-none"
-                  />
-                  <button
-                    onClick={() => handleAddMiniTask(card._id)}
-                    className="mt-2 w-full bg-gray-100 hover:bg-gray-300 text-black px-3 py-2 rounded-lg shadow-md"
-                  >
-                    Add mini task
-                  </button>
+
+                  {/*  */}
+                  <div className="bg-gray-50 rounded-lg shadow-sm">
+                    {/* dynamic card here */}
+
+                    <div className="space-y-2 ">
+                      {data?.task?.subcards[i]?.miniTasks?.map(
+                        (minicard: any) => (
+                          <div
+                            key={minicard._id}
+                            className={`p-3 rounded-lg shadow-md ${minicard.color} flex justify-between items-center`}
+                          >
+                            {minicard.title}
+                            <div className="cursor-pointer">
+                              <FaPencil />
+                            </div>
+                          </div>
+                        )
+                      )}
+                    </div>
+                  </div>
+                  {/* add btn  */}
+                  <div className="mt-6 w-64 mb-4 p-3  rounded-md shadow-md">
+                    <div className="">
+                      <input
+                        type="text"
+                        placeholder="Add inside mini task..."
+                        value={miniTaskInputs[card._id] || ""}
+                        onChange={(e) =>
+                          handleInputChange(card._id, e.target.value)
+                        }
+                        className="w-full px-3 py-2 text-black rounded-md shadow-md hover:bg-gray-50  focus:outline-none focus:ring-0 border-none focus:border-none"
+                      />
+                      <button
+                        onClick={() => handleAddMiniTask(card._id)}
+                        className="mt-2 w-full bg-gray-100 hover:bg-gray-300 text-black px-3 py-2 rounded-lg shadow-md"
+                      >
+                        Add mini task
+                      </button>
+                    </div>
+                  </div>
                 </div>
               </div>
+            ))}
+            {/* end of Card Section */}
+
+            {/* Add subtask card */}
+            <div className="w-72 h-40 bg-white text-black p-6 flex-shrink-0 rounded-lg flex flex-col gap-4">
+              <input
+                type="text"
+                placeholder="Add sub task..."
+                value={newTitle}
+                onChange={(e) => setNewTitle(e.target.value)}
+                className="px-3 py- py-2 rounded-md text-black  shadow-md bg-gray-50  focus:outline-none focus:ring-0 border-none focus:border-none"
+              />
+              <button
+                onClick={handleAddSubTask}
+                className="w-full bg-gray-100 hover:bg-gray-300 text-black px-3 py-2 rounded-lg shadow-md"
+              >
+                Add sub task
+              </button>
             </div>
           </div>
-          ))}
-          {/* end of Card Section */}
-
-          {/* Add subtask card */}
-          <div className="w-72 h-40 bg-gradient-to-r from-blue-500 to-blue-600 text-white p-6 flex-shrink-0 rounded-lg flex flex-col justify-between">
-            <input
-              type="text"
-              placeholder="Add sub task..."
-              value={newTitle}
-              onChange={(e) => setNewTitle(e.target.value)}
-              className="px-3 py-2 rounded-md text-black"
-            />
-            <button
-              onClick={handleAddSubTask}
-              className=" w-full bg-blue-700 hover:bg-blue-800 text-white px-3 py-2 rounded-lg mt-2"
-            >
-              Add sub task
-            </button>
-          </div>
         </div>
-       </div>
       </main>
     </>
   );
