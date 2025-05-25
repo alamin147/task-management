2e18dc6349da86c2e50d45de526084d7899b14f8
alamin147 <alamin.14780@gmail.com>
2025-05-25 20:50:22 +0600

    update in ui components

--- client/src/provider/SidebarProvider.tsx
+++
@@ -1,8 +0,0 @@
-import { getUserInfo } from "../components/auth/utils/authUlits";
-import Rightsidebar from "../components/rightSidebar/RightSidebar";
-
-const SidebarProvider = () => {
-  const user = getUserInfo();
-  return <>{user && <Rightsidebar user={user} />}</>;
-};
-export default SidebarProvider;
